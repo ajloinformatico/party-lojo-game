8cd64fc8e6c42443193c7eded1b1011b5ed6f918
ajloinformatico <ajloinformatico@gmail.com>
2021-08-03 23:17:00 +0200

    refactor asks dto

--- app/app/src/main/java/com/example/party_lojo_game/data/AskTypeBO.kt
+++ app/app/src/main/java/com/example/party_lojo_game/data/AskTypeBO.kt
@@ -3,5 +3,7 @@ package com.example.party_lojo_game.data
 enum class AskTypeBO(type: String) {
     BEBE_QUIEN("bebe quien"),
     YO_NUNCA("yo nunca"),
-    VERDAD_O_RETO("verdad o reto")
+    VERDAD_O_RETO("verdad o reto"),
+    UNKNONW("")
+
 }
--- app/app/src/main/java/com/example/party_lojo_game/data/remote/ResourceDTO.kt
+++ app/app/src/main/java/com/example/party_lojo_game/data/remote/ResourceDTO.kt
@@ -1,5 +1,10 @@
 package com.example.party_lojo_game.data.remote
 
+import com.example.party_lojo_game.data.AskTypeBO
+import com.example.party_lojo_game.data.AsksBO
+import com.example.party_lojo_game.data.Constants.BEBE_QUIEN_DTO_TYPE
+import com.example.party_lojo_game.data.Constants.VERDAD_O_RETO_TYPE
+import com.example.party_lojo_game.data.Constants.YO_NUNCA_DTO_TYPE
 import com.example.party_lojo_game.data.local.BebeQuienDBO
 import com.example.party_lojo_game.data.local.VerdadOretoDBO
 import com.example.party_lojo_game.data.local.YoNuncaDBO
@@ -12,33 +17,57 @@ data class ResourceDTO(
     @SerializedName("text") val text: String?
 ): Serializable
 
+//TODO: REFACTOR THIS
 fun ResourceDTO.toDBO(): Any{
     if (!this.id.isNullOrEmpty() and !this.text.isNullOrEmpty() and !this.type.isNullOrEmpty()){
         when (this.type) {
-            "Bebe quien" -> {
+            BEBE_QUIEN_DTO_TYPE -> {
                 return BebeQuienDBO(
                     id = this.id!!.toLong(),
                     text = this.text!!
                 )
             }
-            "Yo nunca" -> {
+
+            YO_NUNCA_DTO_TYPE -> {
                 return YoNuncaDBO(
                     id = this.id!!.toLong(),
                     text = this.text!!
                 )
             }
-            "Verdad o reto" -> {
+
+            VERDAD_O_RETO_TYPE -> {
                 return VerdadOretoDBO(
                     id = this.id!!.toLong(),
                     text = this.text!!
                 )
             }
+
             else -> {
                 return false
+            }
+        }
+    }
+
+    return false
+}
+
+fun ResourceDTO.toBO(): Any{
+    this.id?.let { id ->
+        this.text?.let { text ->
+            this.type?.let { type ->
+                AsksBO(
+                    id = id.toLong(),
+                    text = text,
+                    type = when (this.type) {
+                        BEBE_QUIEN_DTO_TYPE -> AskTypeBO.BEBE_QUIEN
+                        YO_NUNCA_DTO_TYPE -> AskTypeBO.YO_NUNCA
+                        VERDAD_O_RETO_TYPE -> AskTypeBO.VERDAD_O_RETO
+                        else -> AskTypeBO.UNKNONW
+                    }
+                )
             }
         }
     }
     return false
 }
 
-//TODO MAPPER TO BO
