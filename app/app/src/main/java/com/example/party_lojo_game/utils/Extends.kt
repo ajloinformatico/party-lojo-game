323c78862a357522163e949047c5c64d028e29fd
ANTONIOJLO <ANTONIOJLO@ext.inditex.com>
2023-01-10 16:57:17 +0100

    Replace Extends.kt to ViewExtensions.kt

--- app/app/src/main/java/com/example/party_lojo_game/utils/Extends.kt
+++
@@ -1,7 +0,0 @@
-package com.example.party_lojo_game.utils
-
-import android.view.View
-
-fun View.hide() = View.GONE
-
-fun View.show() = View.VISIBLE
---
+++ app/app/src/main/java/com/example/party_lojo_game/utils/ViewExtensions.kt
@@ -0,0 +1,9 @@
+package com.example.party_lojo_game.utils
+
+import android.view.View
+
+fun View.gone() = View.GONE
+
+fun View.show() = View.VISIBLE
+
+fun View.doInvisible() = View.INVISIBLE
