484f6a210ccf0a46e0f626ddd277a772a7d0a725
ANTONIOJLO <ANTONIOJLO@ext.inditex.com>
2023-02-04 18:43:00 +0100

    Remove not necessary dialogs

--- app/app/src/main/java/com/example/party_lojo_game/MainActivity.kt
+++ app/app/src/main/java/com/example/party_lojo_game/MainActivity.kt
@@ -7,40 +7,40 @@ import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import com.example.party_lojo_game.data.constants.Constants
 import com.example.party_lojo_game.databinding.ActivityMainBinding
 import com.example.party_lojo_game.ui.viewmodel.MainViewModel
-import com.example.party_lojo_game.data.constants.Constants
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
 
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var location: String
-
+    private var binding: ActivityMainBinding? = null
+    private var location: String = ""
     private val mainViewModel by lazy {
-        ViewModelProvider(this).get(MainViewModel::class.java)
+        ViewModelProvider(this)[MainViewModel::class.java]
     }
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding?.root?.let(::setContentView)
 
         //find nav controller
         navController = Navigation.findNavController(
             this, R.id.main_activity__nav_host_fragment
         )
 
-        // TODO WHY I DO IT ????
-        //Set ActionBar
+//        // TODO WHY I DO IT ????
+//        // Set ActionBar
 //        setSupportActionBar(binding.activityMainCustomToolbar.customToolbar)
 //        binding.activityMainCustomToolbar.customToolbar.collapseIcon =
 //            ContextCompact.getDrawable(this, R.drawable.ic_icon)
 //        supportActionBar?.title = ""
-//
+
         //Get response and save on database
         mainViewModel.getRemoteResponse()
 
@@ -62,13 +62,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    // TODO REPLACE THIS
     @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
         when (this.location) {
             Constants.HOME_PAGE_LOCATION -> {
-                //listeners on dialogs are dialog and id
-                //TODO METHOD TO DO ALL OF THIS
                 val dialog = AlertDialog.Builder(this)
                 dialog.setMessage(this.resources.getString(R.string.exit_app))
                     .setTitle(this.resources.getString(R.string.exit_app_title))
@@ -83,32 +80,21 @@ class MainActivity : AppCompatActivity() {
 
 
             }
-            Constants.CONFIG_PLAYERS_LOCATION -> {
-                val dialog = AlertDialog.Builder(this)
-                dialog.setMessage(this.resources.getString(R.string.cancel_config))
-                    .setTitle(this.resources.getString(R.string.important_config))
-                    .setIcon(ContextCompat.getDrawable(this, R.drawable.ic_warning))
-                    .setPositiveButton(R.string.dialog_si) { _, _ -> super.onBackPressed() }
-                    .setNegativeButton(R.string.dialog_no) { _, _ -> /*no-loop*/ }
-                dialog.create()
-                dialog.show()
-            }
-
             Constants.ON_GAME_PLAY -> {
                 val dialog = AlertDialog.Builder(this)
                 dialog.setMessage(this.resources.getString(R.string.on_play_back))
                     .setTitle(this.resources.getString(R.string.important_config))
                     .setIcon(ContextCompat.getDrawable(this, R.drawable.ic_warning))
-                    .setPositiveButton(R.string.dialog_si) { _, _ -> super.onBackPressed() }
-                    .setNegativeButton(R.string.dialog_no) { _, _ -> /*no-loop*/ }
+                    .setPositiveButton(R.string.dialog_si) { _, _ ->
+                        super.onBackPressed()
+                    }
+                    .setNegativeButton(R.string.dialog_no) { _, _ -> /* no-op */ }
                 dialog.create()
                 dialog.show()
             }
             else -> {
-                // TODO REPLACE THIS DEPRECATED
                 super.onBackPressed()
             }
         }
-
     }
 }
